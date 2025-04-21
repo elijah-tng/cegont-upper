7f1085d2430c8b257a640994a38990c62b058f64
Tripleo <tripleo@tripleo.milkyway>
2025-04-21 00:30:18 -0400

    Add pointless DEBUG_ON flag

--- src/main/java/tripleo/elijah_fluffy/util/SimplePrintLoggerToRemoveSoon.java
+++ src/main/java/tripleo/elijah_fluffy/util/SimplePrintLoggerToRemoveSoon.java
@@ -1,80 +1,34 @@
 package tripleo.elijah_fluffy.util;
 
-import org.jetbrains.annotations.*;
 
 public class SimplePrintLoggerToRemoveSoon {
-	@Contract(pure = true)
+
+	private static final boolean DEBUG_ON = false;
+
 	public static void println_err(final String aS) {
-		//System.err.println(aS);
+		if (DEBUG_ON) System.err.println(aS);
 	}
 
-	public static void println_err_2(final String aS) {
-		// System.err.println(String.valueOf(aS));
-	}
-
-	@Contract(pure = true) // ??
-	public static void println_err_3(final String aS) {
-		//System.err.println(aS);
-	}
-
-	@Contract(pure = true)
-	public static void println_err2(final String aS) {
-		//System.err.println(""+aS);
-	}
-
-	@Contract(pure = true)
 	public static void println_out(final String aS) {
-//		System.out.println(aS);
+		if (DEBUG_ON) System.out.println(aS);
 	}
 
-	@Contract(pure = true)
-	public static void println_out_2(final Object aS) {
-		// System.out.println("" + aS);
-	}
-
-	public static void println_out_3(final String aS) {
-//		System.out.println(aS);
-	}
-
-	@Contract(pure = true)
 	public static void println2(final String aS) {
-//		System.out.println(""+aS);
-	}
-
-	public static void println_err_4(final Object aS) {
-		//System.err.println(""+aS);
-	}
-
-	public static void println_out_4(final Object aS) {
-		//System.out.println(aS);
-	}
-
-	public static void println_out_4(final int code, final String message) {
-		if (false) {
-			println_out_4(code + " " + message);
-		}
-	}
-
-/*
-	public static void println_err(final String aS) {
-//		System.err.println(aS);
-	}
-
-	public static void println_out(final String aS) {
-//		System.out.println(aS);
-	}
-
-  public static void println2(final String aS) {
-//		System.out.println(""+aS);
+		if (DEBUG_ON) System.out.println("" + aS);
 	}
 
 	public static void println_err2(final String aS) {
-//		System.err.println("** println_err2 "+aS);
+		if (DEBUG_ON) System.err.println("" + aS);
 	}
 
 	public static void println_out_2(final String aS) {
-//		System.out.println(""+aS);
-  }
-*/
+		if (DEBUG_ON) System.out.println("" + aS);
+	}
 
+	/*
+	 * Make a note of workarounds
+	 */
+	public static void ilf(final String aS) {
+		System.out.println("[ilf] " + aS);
+	}
 }
