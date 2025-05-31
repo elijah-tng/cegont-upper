5bd9c4c219a7378d0511493031d2bc5484a95218
Tripleo <tripleo@tripleo.milkyway>
2025-05-30 21:28:56 -0400

    Enhance LocatableString with #getLocatable and #hasLocation
    Also make ourselves happy by not using inheritance

--- src/main/java/tripleo/elijah_fluffy/xlang/LocatableString.java
+++ src/main/java/tripleo/elijah_fluffy/xlang/LocatableString.java
@@ -1,7 +1,9 @@
 package tripleo.elijah_fluffy.xlang;
 
+import tripleo.elijah_fluffy.diagnostic.ElLocatable;
 import tripleo.vendor.antlr277.Token;
 
+import java.io.File;
 import java.util.Objects;
 
 public interface LocatableString {
@@ -10,7 +12,42 @@ public interface LocatableString {
 			 @Override public String asLocatableString() {
 				return aToken.getText();
 			}
-		};
+
+			 @Override
+			 public ElLocatable getLocatable() {
+				 return new ElLocatable() {
+					 @Override
+					 public int getLine() {
+						 return aToken.getLine();
+					 }
+
+					 @Override
+					 public int getColumn() {
+						 return aToken.getColumn();
+					 }
+
+					 @Override
+					 public int getLineEnd() {
+						 return aToken.getLine();
+					 }
+
+					 @Override
+					 public int getColumnEnd() {
+						 return aToken.getColumn()+asLocatableString().length(); // ?!
+					 }
+
+					 @Override
+					 public File getFile() {
+						 return new File(aToken.getFilename());
+					 }
+				 };
+			 }
+
+			 @Override
+			 public boolean hasLocation() {
+				 return true;
+			 }
+		 };
 	}
 
 	static LocatableString of(String aAbsolutePath) {
@@ -19,10 +56,24 @@ public interface LocatableString {
 			public String asLocatableString() {
 				return aAbsolutePath;
 			}
+
+			@Override
+			public ElLocatable getLocatable() {
+				return null;
+			}
+
+			@Override
+			public boolean hasLocation() {
+				return false;
+			}
 		};
 	}
 
 	String asLocatableString();
+	// Token asLocatableToken();
+	// boolean isToken();
+	ElLocatable getLocatable();
+	boolean hasLocation();
 
 	default boolean sameString(String aName) {
 		return Objects.equals(aName, asLocatableString());
