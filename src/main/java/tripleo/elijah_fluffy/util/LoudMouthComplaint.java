659782e7043a10a661eb254a5be7d72bd8257ea6
tripleo1 <50311859+tripleo1@users.noreply.github.com>
2026-06-26 17:51:41 +0000

    Manually pull LoudMouthComplaint @ 8ba17193

---
+++ src/main/java/tripleo/elijah_fluffy/util/LoudMouthComplaint.java
@@ -0,0 +1,33 @@
+package tripleo.elijah_fluffy.util;
+
+public class LoudMouthComplaint {
+	private static final boolean DEBUG_ON = false;
+
+	public static void println_err(final String aS) {
+		if (DEBUG_ON) System.err.println(aS);
+	}
+
+	public static void println_out(final String aS) {
+		if (DEBUG_ON) System.out.println(aS);
+	}
+
+	public static void println2(final String aS) {
+		if (DEBUG_ON) System.out.println("" + aS);
+	}
+
+	// todo write a parser and push it into sw.else.
+	public static void println_err2(final String aS) {
+		if (DEBUG_ON) System.err.println("" + aS);
+	}
+
+	public static void println_out_2(final String aS) {
+		if (DEBUG_ON) System.out.println("" + aS);
+	}
+
+	/*
+	 * Make a note of workarounds
+	 */
+	public static void ilf(final String aS) {
+		System.out.println("[ilf] " + aS);
+	}
+}
